f86d8e06a5c356e30ee4e41126d4cbfea23b9976
Brunno Khouri <brunnoborges@lugarh.com.br>
2024-01-15 10:23:13 -0300

    Remove Mocks and Modify methods

--- 03_RestWithASPNETUdemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ 03_RestWithASPNETUdemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -1,5 +1,6 @@
 ﻿using RestWithASPNETUdemy.Model;
 using RestWithASPNETUdemy.Model.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -18,42 +19,70 @@ namespace RestWithASPNETUdemy.Services.Implementations
 
         public Person Create(Person person)
         {
+            try 
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch(Exception) 
+            {
+                throw;
+            }
             return person;        
         }
 
         public void Delete(long id)
         {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
 
         }
 
         public List<Person> FindAll()
-        {
-
-           
+        {           
             return _context.Persons.ToList();
         }
 
 
         public Person FindById(long id)
         {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Brunno",
-                LastName = "Borges",
-                Address = "Colombo - Parana - Brasil",
-                Gender = "Masculino"
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id == id);
         }
 
         public Person Update(Person person)
         {
-            return person;
-        }    
+            if (!Exists(person.Id)) return new Person();
 
-        private long IncrementAndGet()
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return person;
+        }
+
+        private bool Exists(long id)
         {
-            return Interlocked.Increment(ref count);
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
